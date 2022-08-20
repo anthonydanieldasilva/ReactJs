79ae1cafbb390e2e72e21effbfc6184ba1ea12bd
anthony <anthonydasilva2012@gmail.com>
2022-08-20 10:26:03 -0300

    Barra de munù simples

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import './NavBar.css'
+import Button from 'react-bootstrap/Button';
+
+
+export const NavBar = () => {
+    return(
+        <div className="headerContainer">
+            <div className="headerLogo" >
+                <h1>LOGO</h1>
+            </div>
+            <nav className="navLinksContainers" >
+                <Button variant="dark m-2">Alineaciòn</Button>
+                <Button variant="dark m-2">Balanceo</Button>
+                <Button variant="dark m-2">Reparaciones</Button>
+                <Button variant="primary m-2">Login</Button>
+            </nav>
+        </div>
+    )
+}
